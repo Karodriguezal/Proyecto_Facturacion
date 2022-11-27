23a930549a5c0a2e70dc37ebfefc6e14fef5d8e4
Karodriguezal <Maca*2921>
2022-11-27 13:26:53 -0500

    first commit

---
+++ src/test/java/com/grupo4/proyecto_facturacion/ProyectoFacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.proyecto_facturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
